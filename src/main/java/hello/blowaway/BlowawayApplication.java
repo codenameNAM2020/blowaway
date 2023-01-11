254fde61d1357776137cd975e734561ef7be305e
codenameNAM2020 <codenam2020@gmail.com>
2023-01-11 09:58:32 +0900

    Initial commit

---
+++ src/main/java/hello/blowaway/BlowawayApplication.java
@@ -0,0 +1,13 @@
+package hello.blowaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlowawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlowawayApplication.class, args);
+	}
+
+}
