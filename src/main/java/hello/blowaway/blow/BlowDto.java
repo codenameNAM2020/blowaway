3a00539fc7abdb4dc92b947ef067bb1616b0b80a
codenameNAM2020 <codenam2020@gmail.com>
2023-02-23 10:39:32 +0900

    fix : db column 변경에 따른 수정

--- src/main/java/hello/blowaway/blow/BlowDto.java
+++ src/main/java/hello/blowaway/blow/BlowDto.java
@@ -10,9 +10,9 @@ import java.time.LocalDateTime;
 public class BlowDto {
 
     private int wid;                // 글 Id
-    private int uid;                // 사용자 Id
+    private int uno;                // 사용자 번호
     private String name;            // 사용자 이름
-    private String content;        // 내용
+    private String content;         // 내용
     private LocalDateTime cdate;    // 작성일
     private LocalDateTime udate;    // 수정일
     private int hit;                // 조회수
